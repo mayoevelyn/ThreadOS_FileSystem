6474046eccdfb7acf7603e8a6432b93fbff6b0e4
Bryan Veneruso <baeshra@yahoo.com>
2018-03-10 15:04:31 -0800

    Added some helper methods and comments for Inode

--- Inode.java
+++ Inode.java
@@ -1,59 +1,71 @@
 public class Inode {
-   private final static int iNodeSize = 32;       // fix to 32 bytes
-   private final static int directSize = 11;      // # direct pointers
+	private final static int iNodeSize = 32;       // fix to 32 bytes
+	private final static int directSize = 11;      // # direct pointers
 
-   public int length;                             // file size in bytes
-   public short count;                            // # file-table entries pointing to this
-   public short flag;                             // 0 = unused, 1 = used, ...
-   public short direct[] = new short[directSize]; // direct pointers
-   public short indirect;                         // a indirect pointer
+	public int length;                             // file size in bytes
+	public short count;                            // # file-table entries pointing to this
+	public short flag;                             // 0 = unused, 1 = used, ...
+	public short direct[] = new short[directSize]; // direct pointers
+	public short indirect;                         // a indirect pointer
 
-   Inode( ) {                                     // a default constructor
-      length = 0;
-      count = 0;
-      flag = 1;
-      for ( int i = 0; i < directSize; i++ )
-         direct[i] = -1;
-      indirect = -1;
-   }
+	Inode( ) {                                     // a default constructor
+		length = 0;
+		count = 0;
+		flag = 1;
+		for ( int i = 0; i < directSize; i++ )
+			direct[i] = -1;
+		indirect = -1;
+	}
 
-   Inode( short iNumber ) {                       // retrieving inode from disk
-      // design it by yourself.
-      // from slide
-      int blockNumber = 1 + iNumber / 16;
-      byte[] data = new byte[Disk.blockSize];
-      SysLib.rawread( blockNumber, data );
-      int offset = ( iNumber % 16 ) * 32;
+	//helper so you can use ints in other classes
+	Inode(int iNumber) {
+		this((short)iNumber);
+	}
 
-      length = SysLib.bytes2int( data, offset );
-      offset += 4;
-      count = SysLib.bytes2short( data, offset );
-      offset += 2;
-      flag = SysLib.bytes2short( data, offset );
-      offset += 2;
+	//Loads iNode from disk
+	Inode( short iNumber ) {
+		int blockNumber = 1 + iNumber / 16;
+		byte[] data = new byte[Disk.blockSize];
+		SysLib.rawread( blockNumber, data );
+		int offset = ( iNumber % 16 ) * 32;
 
-      for(int i = 0; i < 11; ++i)
-      {
-         direct[i] = SysLib.bytes2short(data, offset);
-      }
-   }
+		length = SysLib.bytes2int( data, offset );
+		offset += 4;
+		count = SysLib.bytes2short( data, offset );
+		offset += 2;
+		flag = SysLib.bytes2short( data, offset );
+		offset += 2;
 
-   // returns void according to slide?
-   int toDisk( short iNumber ) {                  // save to disk as the i-th inode
+		for(int i = 0; i < 11; ++i) {
+			direct[i] = SysLib.bytes2short(data, offset);
+		}
+	}
 
-   }
+	//save to disk as the given node
+	int toDisk( short iNumber ) {
 
-   short getIndexBlockNumber() {
+	}
+	
+	//helper method
+	int toDisk(int iNumber) {
+		return toDisk((short) iNumber);
+	}
+	
+	//Returns the number of the block that contains the indirect block for this iNode
+	short getIndexBlockNumber() {
+		return indirect;
+	}
+	
+	//Sets the location of the indirect block
+	boolean setIndexBlock( short indexBlockNumber ) {
 
-   }
-
-   boolean setIndexBlock( short indexBlockNumber ) {
-
-   }
-
-   short findTargetBlock( int offset ) {
-      // from slides
-      // blk = offset/512;
-      // if(blk >= 11) scan the index block
-   }
+	}
+	
+	//Finds the block that a seek pointer at this offset would be referencing
+	short findTargetBlock( int offset ) {
+	// from slides
+	// blk = offset/512;
+	// if(blk >= 11) scan the index block
+	}
+	
 }
