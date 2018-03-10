ef2ca2a120fc63456cbaf7bc3dbd18e0278e7e33
emahasin3 <emahasin@uw.edu>
2018-03-10 15:53:14 -0800

    superblock
    done

--- Superblock.java
+++ Superblock.java
@@ -22,44 +22,81 @@ public class SuperBlock {
 		}
 	}
 
+	// wrapper
+	void format() {
+		format(defaultInodeBlocks);
+	}
+
 	void format( int inodeSize ) {
 		// 4 blocks for inodes if 64 inodes
 		this.totalInodes = inodeSize;
 
-		for(short var2 = 0; var2 < this.totalInodes; ++var2) {
-			Inode var3 = new Inode();
-			var3.flag = 0;
-			var3.toDisk(var2);
+		for(short i = 0; i < this.totalInodes; ++i) {
+			Inode newInode = new Inode();
+			newInode.flag = 0;
+			newInode.toDisk(i);
 		}
 
-		this.freeList = 2 + this.totalInodes * 32 / 512;
+		freeList = 2 + ((totalInodes*32)/512);
 
-		for(int var5 = this.freeList; var5 < this.totalBlocks; ++var5) {
-			byte[] var6 = new byte[512];
 
-			for(int var4 = 0; var4 < 512; ++var4) {
-				var6[var4] = 0;
+		for(int i = freeList; i < totalBlocks; ++i) {
+			byte[] newBlock = new byte[512];
+
+			for(int j = 0; j < 512; ++j) {
+				newBlock[j] = 0;
 			}
 
-			SysLib.int2bytes(var5 + 1, var6, 0);
-			SysLib.rawwrite(var5, var6);
+			SysLib.int2bytes(i + 1, newBlock, 0);
+			SysLib.rawwrite(i, newBlock);
 		}
 
 		this.sync();
 	}
 
-	// Write bak totalBlocks, indoeBlocks, and freeList to disk
+	// Write back totalBlocks, inodeBlocks, and freeList to disk
 	void sync() {
-
+		byte[] block = new byte[512];
+		SysLib.int2bytes(this.totalBlocks, block, 0);
+		SysLib.int2bytes(this.totalInodes, block, 4);
+		SysLib.int2bytes(this.freeList, block, 8);
+		SysLib.rawwrite(0, block);
+		SysLib.cerr("Superblock synchronized\n");
 	}
 
 	// Dequeue the top block from the free list
 	public int getFreeBlock() {
-
+		// check that freelist head block is valid
+		if (freeList != -1) {
+			byte[] emptyBlock = new byte[512];
+			SysLib.rawread(freeList, emptyBlock);
+			freeList = SysLib.bytes2int(emptyBlock, 0);
+			SysLib.int2bytes(0, emptyBlock, 0);
+			SysLib.rawwrite(freeList, emptyBlock);
+		}
+		// return the free block number
+		return freeList;
 	}
 
 	// Enqueue a given block to the end of the free list
 	public boolean returnBlock ( int blockNumber ) {
+		// check for valid block number
+		if (blockNumber < 0) {
+			return false;
+		} else {
+			byte[] emptyBlock = new byte[512];
 
+			// empty block by setting each byte as 0
+			for(int i = 0; i < 512; ++i) {
+				emptyBlock[i] = 0;
+			}
+
+			// get block number in bytes to write to disk
+			SysLib.int2bytes(freeList, emptyBlock, 0);
+			SysLib.rawwrite(blockNumber, emptyBlock);
+			// set the new empty block to
+			freeList = blockNumber;
+			return true;
+		}
 	}
 }
